055846c0c0f7862623cc00ef4a3a7a2062d96b03
William Caetano <williamcaetano14@gmail.com>
2018-01-11 15:09:51 -0200

    Created basic test file

---
+++ test/test_will_doc_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'will_doc_project'
+
+class WillDocProjectTest < Minitest::Test
+  def test_start
+    assert_equal "Not implemented yet", WillDocProject.start
+  end
+end
